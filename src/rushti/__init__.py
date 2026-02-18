97716af610feec0ee3c63fc06fd0f9d862258658
nicolasbisurgi <nbisurgi@cubewise.com>
2026-02-18 08:57:40 -0300

    Bump version to 2.0.0b13

--- src/rushti/__init__.py
+++ src/rushti/__init__.py
@@ -5,7 +5,7 @@ This package provides tools for executing TM1 TI processes in parallel
 with dependency management, checkpoint/resume support, and execution logging.
 """
 
-__version__ = "2.0.0b12"
+__version__ = "2.0.0b13"
 __app_name__ = "RushTI"
 
 # Core exports for programmatic use
